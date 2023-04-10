3ca9a660b031d1593fe00081b4903db193fcfce6
Matthieu MOREL <matthieu.morel35@gmail.com>
2023-04-10 11:17:14 +0200

    Update common_linux.go

--- internal/common/common_linux.go
+++ internal/common/common_linux.go
@@ -77,7 +77,8 @@ func BootTimeWithContext(ctx context.Context) (uint64, error) {
 		}
 
 		currentTime := time.Now().UnixNano() / int64(time.Second)
-		t := currentTime - info.Uptime
+		//nolint: unconvert
+		t := currentTime - int64(info.Uptime)
 		return uint64(t), nil
 	}
 	if err != nil {
